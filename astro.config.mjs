a69b4b27ab606aa16a93f6739c1cafc1bc68c901
vodyaraman <130937079+vodyaraman@users.noreply.github.com>
2025-01-19 10:58:46 +0300

    init-reusable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url).pathname,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @import "@/styles/common.scss"; 
+          @import "@/styles/color-variables.scss";
+          @import "@/styles/global.scss";
+          `
+        }
+      }
+    }
+  },
+});
