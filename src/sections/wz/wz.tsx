8c5d4c6e0335311a7efa532225c1889c06dd5fa5
vodyaraman <a.vodyaraman@gmail.com>
2025-02-11 00:23:20 +0300

    wz

--- src/sections/wz/wz.tsx
+++ src/sections/wz/wz.tsx
@@ -1,45 +1,49 @@
 import { useState } from 'react';
+import "./wz.scss"
 
 const TTKCalculator = () => {
-  const [damage, setDamage] = useState('');
-  const [rpm, setRpm] = useState('');
-  const [ttk, setTtk] = useState<number | null>(null);
+    const [damage, setDamage] = useState('');
+    const [rpm, setRpm] = useState('');
+    const [ttk, setTtk] = useState<number | null>(null);
 
-  const calculateTTK = () => {
-    const health = 300; // Фиксированное значение здоровья цели
-    const shotsNeeded = Math.ceil(health / (parseFloat(damage) || 1));
-    const timePerShot = 60 / (parseFloat(rpm) || 1);
-    setTtk((shotsNeeded - 1) * timePerShot);
-  };
+    const calculateTTK = () => {
+        const health = 300; // Фиксированное значение здоровья цели
+        const shotsNeeded = Math.ceil(health / (parseFloat(damage) || 1));
+        const timePerShot = 60 / (parseFloat(rpm) || 1);
+        setTtk((shotsNeeded - 1) * timePerShot);
+    };
 
-  return (
-    <div className="ttk-calculator">
-      <h2>Калькулятор TTK</h2>
-      <div>
-        Урон за выстрел:
-        <input
-          type="number"
-          value={damage}
-          onChange={(e) => setDamage(e.target.value)}
-        />
-      </div>
-      
-      <div>
-        Темп стрельбы (выстрелов в минуту):
-        <input
-          type="number"
-          value={rpm}
-          onChange={(e) => setRpm(e.target.value)}
-        />
-      </div>
-      
-      <button onClick={calculateTTK}>Рассчитать TTK</button>
-      
-      {ttk !== null && (
-        <p>Время для убийства: <strong>{ttk.toFixed(3)} сек</strong></p>
-      )}
-    </div>
-  );
+    return (
+        <section className='main-wz'>
+            <div className="ttk-calculator">
+                <h2>Калькулятор TTK</h2>
+                <div>
+                    Урон за выстрел:
+                    <input
+                        type="number"
+                        value={damage}
+                        onChange={(e) => setDamage(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    Темп стрельбы (выстрелов в минуту):
+                    <input
+                        type="number"
+                        value={rpm}
+                        onChange={(e) => setRpm(e.target.value)}
+                    />
+                </div>
+
+                <button onClick={calculateTTK}>Рассчитать TTK</button>
+
+                {ttk !== null && (
+                    <p>Время для убийства: <strong>{ttk.toFixed(3)} сек</strong></p>
+                )}
+            </div>
+        </section>
+
+    );
 };
 
 export default TTKCalculator;
